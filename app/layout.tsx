ca2a2db752604ae5845008f15b16b872673d4651
ValentinBytenskiy <valentin.bytenskiy@ravendb.net>
2026-04-18 17:46:52 +0300

    Content in its place

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next'
-import { Rubik_Spray_Paint, Poller_One } from 'next/font/google'
+import { Rubik_Spray_Paint, Frank_Ruhl_Libre } from 'next/font/google'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './globals.css'
 
@@ -9,10 +9,10 @@ const rubikSprayPaint = Rubik_Spray_Paint({
   variable: '--font-rubik-spray',
 })
 
-const pollerOne = Poller_One({
+const frankRuhlLibre = Frank_Ruhl_Libre({
   weight: '400',
-  subsets: ['latin'],
-  variable: '--font-poller-one',
+  subsets: ['hebrew'],
+  variable: '--font-frank-ruhl-libre',
 })
 
 export const metadata: Metadata = {
@@ -21,7 +21,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="he" dir="rtl" className={`${rubikSprayPaint.variable} ${pollerOne.variable}`}>
+    <html lang="he" dir="rtl" className={`${rubikSprayPaint.variable} ${frankRuhlLibre.variable}`}>
       <body className="container-fluid">{children}</body>
     </html>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -3,199 +3,232 @@ import BootstrapClient from './BootstrapClient'
 import Question from './Question'
 
 export default function Home() {
-  return (
-    <>
-      <BootstrapClient />
-      <header>
-        <div className="row align-items-center py-5">
-          <div className="text-center py-4 col-9">
-            <h1 className="greeting">יום הולדת 10 של אלכסיי ביטנסקי</h1>
-          </div>
-          <div className="col-3 text-center">
-            <Image className="header_img w-100 h-auto" src="/img/cake.jpg" alt="cake" width={300} height={300} />
-          </div>
-        </div>
-      </header>
+    return (
+        <>
+            <BootstrapClient/>
+            <header>
+                <div className="row align-items-center py-5">
+                    <div className="text-center py-4 col-9">
+                        <h1 className="greeting">יום הולדת 10 של אלכסיי ביטנסקי</h1>
+                    </div>
+                    <div className="col-3 text-center">
+                        <Image className="header_img w-100 h-auto" src="/img/cake.jpg" alt="cake" width={300}
+                               height={300}/>
+                    </div>
+                </div>
+            </header>
 
-      <main role="main" className="container">
-        <Question
-          id="games"
-          questionNumber={1}
-          slides={[
-            {
-              images: [
-                { src: '/img/table-games.jpg', alt: 'table games' },
-                { src: '/img/in-a-house-of-pillows.jpg', alt: 'house of pillows' },
-              ],
-              caption: 'באילו משחקים אלכסיי הכי אוהב לשחק?',
-            },
-            {
-              images: [{ src: '/img/games-collage.png', alt: 'favorite games' }],
-            },
-            {
-              images: [
-                { src: '/img/rope-dragging.jpg', alt: 'rope dragging' },
-                { src: '/img/in-shlulit-playing.jpg', alt: 'playing outside' },
-              ],
-              caption: 'גם משחקי מחשב וגם משחקים מהנים בחיים האמיתיים',
-              captionMdOnly: true,
-            },
-          ]}
-        />
+            <main role="main" className="container">
 
-        <Question
-          id="animals"
-          questionNumber={2}
-          slides={[
-            {
-              images: [
-                { src: '/img/with-hamster.jpg', alt: 'hamster' },
-                { src: '/img/with-George-at-the-door.jpg', alt: 'George' },
-              ],
-              caption: 'כמה חיות יש לאלכסיי בבית?',
-            },
-            {
-              images: [{ src: '/img/4-pigs.jpg', alt: 'guinea pigs' }],
-              caption: 'יש לאלכסיי 6 חיות בבית',
-              captionMdOnly: true,
-            },
-            {
-              images: [
-                { src: '/img/with-a-parrot.jpg', alt: 'parrot' },
-                { src: '/img/with-hamster.jpg', alt: 'hamster' },
-              ],
-              caption: 'אלכסיי אוהב מאוד חיות ונהנה לטפל בהן',
-              captionMdOnly: true,
-            },
-            {
-              images: [
-                { src: '/img/4-pigs.jpg', alt: 'pig 1' },
-                { src: '/img/with-George-at-the-door.jpg', alt: 'pig 2' },
-                { src: '/img/with-hamster.jpg', alt: 'hamster 1' },
-                { src: '/img/with-a-parrot.jpg', alt: 'parrot 1' },
-                { src: '/img/4-pigs.jpg', alt: 'pig 3' },
-                { src: '/img/with-hamster.jpg', alt: 'hamster 2' },
-              ],
-              grid: true,
-            },
-          ]}
-        />
+                <Question
+                    id="family"
+                    questionNumber={1}
+                    slides={[
+                        {
+                            images: [{src: '/img/(1) family/title-all-family.jpg', alt: 'family title'}],
+                        },
+                        {
+                            images: [
+                                {src: '/img/(1) family/with-hamster.jpg', alt: 'with hamster'},
+                                {src: '/img/(1) family/the-Weasley-family.jpg', alt: 'Weasley family'},
+                            ],
+                            caption: 'הם גם חברי משפחה'
+                        },
+                    ]}
+                />
 
-        <Question
-          id="family"
-          questionNumber={3}
-          slides={[
-            {
-              images: [{ src: '/img/all_family.jpg', alt: 'family' }],
-              caption: 'עם מי אלכסיי הכי אוהב לבלות?',
-            },
-            {
-              images: [
-                { src: '/img/table-games.jpg', alt: 'board games' },
-                { src: '/img/in-a-hammock.jpg', alt: 'hammock' },
-              ],
-              caption: 'עם המשפחה, במשחקים, בצחוקים ובזמן ביחד',
-              captionMdOnly: true,
-            },
-            {
-              images: [{ src: '/img/all_family.jpg', alt: 'family' }],
-              caption: 'משפחה טובה הופכת כל יום לחגיגה',
-              captionMdOnly: true,
-            },
-              {
-              images: [{ src: '/img/tiger.jpg', alt: 'tiger' }, { src: '/img/mammoth.jpg', alt: 'mammoth' }],
-              caption: '',
-              captionMdOnly: true,
-            },
-          ]}
-        />
+                <Question
+                    id="favourite-animal"
+                    questionNumber={2}
+                    slides={[
+                        {
+                            images: [
+                                {
+                                    src: '/img/(2) most-favourite-aminal/with-little-George.jpg',
+                                    alt: 'with-little-George'
+                                },
+                                {
+                                    src: '/img/(2) most-favourite-aminal/with-little-George-2.jpg',
+                                    alt: 'with little George'
+                                },
+                            ],
+                            caption: "ג'ורג'"
 
-        <Question
-          id="friends"
-          questionNumber={4}
-          slides={[
-            {
-              images: [
-                { src: '/img/with-a-friend-and-ice-cream.jpg', alt: 'friend ice cream' },
-                { src: '/img/ice-cream-near-the-sea.jpg', alt: 'sea ice cream' },
-              ],
-              caption: 'מה הכי כיף לעשות עם חברים?',
-            },
-            {
-              images: [
-                { src: '/img/table-games.jpg', alt: 'games' },
-                { src: '/img/with-a-friend-and-ice-cream.jpg', alt: 'friend' },
-              ],
-              caption: 'לשחק, לצחוק, לאכול משהו טעים וליהנות יחד',
-              captionMdOnly: true,
-            },
-            {
-              images: [{ src: '/img/ice-cream-near-the-sea.jpg', alt: 'sea' }],
-              caption: 'כשיש חברים טובים, כל יום מרגיש כמו חגיגה',
-              captionMdOnly: true,
-            },
-          ]}
-        />
+                        }, {
+                            images: [
+                                {
+                                    src: '/img/(2) most-favourite-aminal/with-George-at-the-door.jpg',
+                                    alt: 'George at the door'
+                                },
+                                {src: '/img/(2) most-favourite-aminal/with-George.jpg', alt: 'with George'},
+                            ],
+                            caption: "ג'ורג'"
+                        },
+                        {
+                            images: [
+                                {
+                                    src: '/img/(2) most-favourite-aminal/indian-with-George.jpg',
+                                    alt: 'Indian with George'
+                                },
+                                {
+                                    src: '/img/(2) most-favourite-aminal/with-George-embrace.jpg',
+                                    alt: 'with little George'
+                                },
+                            ],
+                            caption: "ג'ורג'"
 
-        <Question
-          id="adventures"
-          questionNumber={5}
-          slides={[
-            {
-              images: [
-                { src: '/img/on-a-pony.jpg', alt: 'pony' },
-                { src: '/img/rope-dragging.jpg', alt: 'rope' },
-              ],
-              caption: 'מה אלכסיי הכי אוהב: לשבת במקום או לצאת להרפתקה?',
-            },
-            {
-              images: [
-                { src: '/img/in-shlulit-running.jpg', alt: 'running' },
-                { src: '/img/in-shlulit-playing.jpg', alt: 'playing' },
-              ],
-              caption: 'אלכסיי אוהב לזוז, לרוץ, לשחק ולגלות דברים חדשים',
-              captionMdOnly: true,
-            },
-            {
-              images: [
-                { src: '/img/with-a-parrot.jpg', alt: 'parrot' },
-                { src: '/img/ice-cream-near-the-sea.jpg', alt: 'sea' },
-              ],
-              caption: 'גם חיות, גם ים וגם המון חוויות',
-              captionMdOnly: true,
-            },
-            {
-              images: [{ src: '/img/in-a-hammock.jpg', alt: 'hammock' }],
-              caption: 'ובסוף גם לנוח כמו אלוף',
-              captionMdOnly: true,
-            },
-          ]}
-        />
+                        },
 
-        <Question
-          id="special"
-          questionNumber={6}
-          slides={[
-            {
-              images: [{ src: '/img/in-a-hat.jpg', alt: 'hat' }],
-              caption: 'מה עושה את אלכסיי מיוחד?',
-            },
-            {
-              images: [
-                { src: '/img/with-George-at-the-door.jpg', alt: 'George at door' },
-                { src: '/img/on-a-pony.jpg', alt: 'pony' },
-              ],
-              caption: 'לב טוב, סקרנות גדולה והמון הומור',
-              captionMdOnly: true,
-            },
-            {
-              images: [{ src: '/img/all_family.jpg', alt: 'family' }],
-              caption: 'אלכסיי בן 10, וזה רק מתחיל!',
-              captionMdOnly: true,
-            },
-          ]}
-        />
-      </main>
-    </>
-  )
+                    ]}
+                />
+
+                <Question
+                    id="games"
+                    questionNumber={3}
+                    slides={[
+                        {
+                            images: [{src: '/img/(3) games/table-games-single-image.jpg', alt: 'table games'}],
+                            caption: 'משחקי שולחן'
+                        },
+                        {
+                            images: [{src: '/img/(3) games/computer-games-single-image.png', alt: 'computer games'}],
+                            caption: 'משחקי מכשב'
+                        },
+                        {
+                            images: [
+                                {src: '/img/(3) games/games-with-pigs-Weasley.jpg', alt: 'games with Weasley'},
+                                {
+                                    src: '/img/(3) games/games-with-pigs-in-a-house-of-pillows.jpg',
+                                    alt: 'house of pillows'
+                                },
+                            ],
+                            caption: "משחקים עם ג'ורג' ושרקנים אחרים"
+                        },
+                        {
+                            images: [
+                                {src: '/img/(3) games/active-bow.jpg', alt: 'bow'},
+                                {src: '/img/(3) games/active-rope-dragging.jpg', alt: 'rope dragging'},
+                            ],
+                            caption: "משחקים אקטיביים"
+                        },
+                        {
+                            images: [
+                                {src: '/img/(3) games/active-in-shlulit-playing.jpg', alt: 'playing outside'},
+                                {src: '/img/(3) games/active-in-shlulit-running.jpg', alt: 'running outside'},
+                            ],
+                            caption: "משחקים אקטיביים"
+                        },
+                    ]}
+                />
+
+                <Question
+                    id="leisure"
+                    questionNumber={4}
+                    slides={[
+                        {
+                            images: [
+                                {src: '/img/(4) other-leasure/Acre-boat-trip.jpg', alt: 'Acre boat trip'},
+                                {src: '/img/(4) other-leasure/Acre-on-a-pony.jpg', alt: 'Acre pony'},
+                            ],
+                            caption: 'טיולי ים ולרקב'
+                        },
+                        {
+                            images: [
+                                {src: '/img/(4) other-leasure/Kratovo-at-a-fireplace.jpg', alt: 'Kratovo fireplace'},
+                                {src: '/img/(4) other-leasure/Kratovo-in-a-hammock.jpg', alt: 'Kratovo hammock'},
+                            ],
+                            caption: 'מנוחה בערסל וליד האח'
+                        },
+                        {
+                            images: [
+                                {src: '/img/(4) other-leasure/ice-cream-near-the-sea.jpg', alt: 'ice cream at the sea'},
+                                {
+                                    src: '/img/(4) other-leasure/ice-cream-with-a-friend.jpg',
+                                    alt: 'ice cream with a friend'
+                                },
+                            ],
+                            caption: 'גלידה עם חברים'
+                        },
+                        {
+                            images: [
+                                {src: '/img/(4) other-leasure/quest-1.jpg', alt: 'quest 1'},
+                                {src: '/img/(4) other-leasure/quest-2.jpg', alt: 'quest 2'},
+                            ],
+                            caption: 'משחקי משימות'
+                        },
+                        {
+                            images: [
+                                {src: '/img/(4) other-leasure/aquapark.jpg', alt: 'aquapark'},
+                                {src: '/img/(4) other-leasure/in-disguise.jpg', alt: 'in disguise'},
+                            ],
+                            caption: 'לאכול בפארק מים ומשחקי תחפושות'
+                        },
+                    ]}
+                />
+
+                <Question
+                    id="animals"
+                    questionNumber={5}
+                    slides={[
+                        {
+                            images: [
+                                {src: '/img/(5) with-animals/feeding-parrots.jpg', alt: 'feeding parrots'},
+                                {src: '/img/(5) with-animals/with-a-parrot.jpg', alt: 'with a parrot'},
+                            ],
+                            caption: 'אינטראקציה עם בעלי חיים קטנים'
+                        },
+                        {
+                            images: [
+                                {src: '/img/(5) with-animals/with-parrot.jpg', alt: 'with parrot'},
+                                {src: '/img/(5) with-animals/with-newborn-pigs.jpg', alt: 'newborn pigs'},
+                            ],
+                            caption: 'אינטראקציה עם בעלי חיים קטנים'
+                        },
+                        {
+                            images: [
+                                {src: '/img/(5) with-animals/with-springer.jpg', alt: 'with springer'},
+                                {src: '/img/(5) with-animals/with-turtle.jpg', alt: 'with turtle'},
+                            ],
+                            caption: 'אינטראקציה עם בעלי חיים קטנים'
+                        },
+                    ]}
+                />
+
+                <Question
+                    id="latest-trip"
+                    questionNumber={6}
+                    slides={[
+                        {
+                            images: [{src: '/img/(6) latest-trip/title-single-image.jpg', alt: 'trip title'}],
+                        },
+                        {
+                            images: [
+                                {src: '/img/(6) latest-trip/in-an-ice-chair.jpg', alt: 'ice chair'},
+                                {src: '/img/(6) latest-trip/bear-sofa.jpg', alt: 'bear on sofa'},
+                            ],
+                        },
+                        {
+                            images: [
+                                {src: '/img/(6) latest-trip/elephant-seal.jpg', alt: 'elephant seal'},
+                                {src: '/img/(6) latest-trip/bear.jpg', alt: 'bear'}
+                            ],
+                        },
+                        {
+                            images: [
+                                {src: '/img/(6) latest-trip/tiger.jpg', alt: 'tiger'},
+                                {src: '/img/(6) latest-trip/mammoth.jpg', alt: 'mammoth'},
+                            ],
+                        },
+                        {
+                            images: [
+                                {src: '/img/(6) latest-trip/pinguin.jpg', alt: 'penguin'},
+                                {src: '/img/(6) latest-trip/walrus.jpg', alt: 'walrus'},
+                            ],
+                        },
+                    ]}
+                />
+
+            </main>
+        </>
+    )
 }
